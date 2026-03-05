1ea12c50d05dd17cef10773d5d9129c7763d1951
YanuarRamadhaniKuswoko <ryanuar867@gmail.com>
2026-03-05 22:36:18 +0700

    Jobsheet 5

--- app/Filament/Resources/Posts/Schemas/PostForm.php
+++ app/Filament/Resources/Posts/Schemas/PostForm.php
@@ -12,6 +12,10 @@ use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\TagsInput;
 use Filament\Forms\Components\Checkbox;
 use Filament\Forms\Components\DateTimePicker;
+use Filament\Schemas\Components\Section;
+use Filament\Support\Icons\Heroicon;
+use Filament\Schemas\Components\Group;
+
 
 
 class PostForm
@@ -20,22 +24,35 @@ class PostForm
     {
         return $schema
             ->components([
-                //
-                TextInput::make('title')->required(),
-                TextInput::make('slug')->required(),
-                Select::make('category_id')
-                ->relationship("category", "name")
-                ->preload()
-                ->searchable(),
-                ColorPicker::make('color'),
-                //MarkdownEditor::make('content'),
-                RichEditor::make('content'),
-                FileUpload::make('image')
-                ->disk('public')
-                ->directory('post'),
-                TagsInput::make('tags'),
-                Checkbox::make('published'),
-                dateTimePicker::make('published_at'),
-            ]);
+                Section::make('Post Details')
+                    ->description('Fill in the details of the post.')
+                    ->icon('heroicon-o-document-text')
+                    ->schema([
+                        Group::make([
+                        TextInput::make('title'),
+                        TextInput::make('slug'),
+                        Select::make('category_id'),
+                        ColorPicker::make('color'),
+                        ])->columns(2),
+                        MarkdownEditor::make('content'),
+                    ])->columnSpan(2),
+
+                Group::make([
+                    Section::make('Image Upload')->schema([
+                        FileUpload::make('image')
+                            ->disk('public')
+                            ->directory('posts'),
+                    ]),
+
+                    Section::make('Meta Information')->schema([
+                        // RichEditor::make('content'),
+                        TagsInput::make('tags'),
+                        Checkbox::make('published'),
+                    ])->columns(1),
+
+                    DateTimePicker::make('published_at'),
+                ]),
+
+            ])->columns(3);
     }
 }
