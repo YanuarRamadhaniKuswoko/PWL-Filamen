6f090a65fc3fb591d6dada1898707963ae10dfdb
YanuarRamadhaniKuswoko <ryanuar867@gmail.com>
2026-03-30 17:56:09 +0700

    Jobsheet 13

--- app/Filament/Resources/Posts/Tables/PostsTable.php
+++ app/Filament/Resources/Posts/Tables/PostsTable.php
@@ -4,7 +4,9 @@ namespace App\Filament\Resources\Posts\Tables;
 
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\DeleteAction;
 use Filament\Actions\EditAction;
+use Filament\Actions\ReplicateAction;
 use Filament\Tables\Table;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Columns\ColorColumn;
@@ -13,6 +15,8 @@ use Filament\Tables\Columns\IconColumn;
 use Filament\Tables\Filters\Filter;
 use Filament\Forms\Components\DatePicker;
 use Filament\Tables\Filters\SelectFilter;
+use Filament\Forms\Components\Checkbox;
+use Filament\Actions\Action;
 
 
 
@@ -71,8 +75,20 @@ class PostsTable
                     ->preload(),
             ])
             ->recordActions([
-                EditAction::make()
+                ReplicateAction::make(),
+                EditAction::make(),
+                DeleteAction::make(),
+                Action::make('status')
+                ->label('status change')
+                ->icon('heroicon-o-check-circle')
+                ->schema([
+                    Checkbox::make('published')
+                    ->default(fn ($record) : bool => $record->published),
             ])
+            ->action(function ($record, $data) {
+                $record->update(['published' => $data['published']]);
+            })
+                ])
             ->toolbarActions([
                 BulkActionGroup::make([
                     DeleteBulkAction::make(),
